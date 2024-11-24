b29e9a7121c66397fbce8cf1ba7dbb4ba258b091
Gustavo Amaral Corrêa <Gubep@icloud.com>
2024-11-24 18:30:48 -0300

    redirecionamento da pagina

--- login.php
+++ login.php
@@ -1,16 +1,12 @@
 <?php
+
+// Efetua a conexao com o banco de dados
+include('config.php');
+
 // Recebe os dados do formulário
 $nome = $_POST["nome"];
 $senha = $_POST["senha"];
 $acao = $_POST["acao"];
-
-// Conecta ao banco de dados
-$con = new mysqli("127.0.0.1", "root", "", "Funko");
-
-// Verifica a conexão
-if ($con->connect_error) {
-    die("Connection failed: " . $con->connect_error);
-}
 
 // Inicia a sessão
 session_start();
@@ -25,8 +21,10 @@ if ($acao == "cadastro") {
     // Executa a SQL
     if ($stmt->execute()) {
         $resposta[] = array("resultado" => "inserido");
+        header('Location: pagina1.html')
     } else {
         $resposta[] = array("resultado" => "erro de inserção");
+        echo json_encode($resposta);
     }
 
     $stmt->close();
@@ -42,14 +40,15 @@ if ($acao == "cadastro") {
     if ($result->num_rows > 0) {
         $_SESSION["nome"] = $nome;
         $resposta[] = array("resultado" => "login bem-sucedido");
+        header('Location: pagina1.html')
     } else {
         $resposta[] = array("resultado" => "nome ou senha incorretos");
+        echo json_encode($resposta);
     }
 
     $stmt->close();
 }
 
 // Retorna a resposta em JSON
-echo json_encode($resposta);
 $con->close();
 ?>
